f46db00ba327336197032c6a2218845508bb3a22
Sébastien Schiesser <sebastien.schiesser@zhdk.ch>
2021-06-15 09:46:23 +0200

    client/server communication for calibration... good idea?

--- rawHID2OSC/rawHID2OSC.c
+++ rawHID2OSC/rawHID2OSC.c
@@ -11,40 +11,40 @@
 const bool debug = true;
 enum machine_state state = STATE_IDLE;
 
-int main ()
+int main()
 {
   int i, r, num;
   char c, buf[64];
 
   // C-based example is 16C0:0480:FFAB:0200
-  r = rawhid_open (1, 0x16C0, 0x0480, 0xFFAB, 0x0200);
-  if (r <= 0)
+  r = rawhid_open(1, 0x16C0, 0x0480, 0xFFAB, 0x0200);
+  if(r <= 0)
   {
     // Arduino-based example is 16C0:0486:FFAB:0200
-    r = rawhid_open (1, 0x1C57, 0x1234, 0xFFAB, 0x0200);
-    if (r <= 0)
+    r = rawhid_open(1, 0x1C57, 0x1234, 0xFFAB, 0x0200);
+    if(r <= 0)
     {
-      printf ("no rawhid device found\n");
+      printf("no rawhid device found\n");
       return -1;
     }
   }
-  printf ("found rawhid device\n");
-  print_help ();
+  printf("found rawhid device\n");
+  display_help();
 
-  while (1)
+  while(1)
   {
     // check if any Raw HID packet has arrived
-    num = rawhid_recv (0, buf, 64, 220);
-    if (num < 0)
+    num = rawhid_recv(0, buf, 64, 220);
+    if(num < 0)
     {
-      printf ("\nerror reading, device went offline\n");
-      rawhid_close (0);
+      printf("\nerror reading, device went offline\n");
+      rawhid_close(0);
       return 0;
     }
-    if (num > 0)
+    if(num > 0)
     {
-      printf ("\nrecv %d bytes:\n", num);
-      parse_packet (buf);
+      printf("\nrecv %d bytes:\n", num);
+      parse_packet(buf);
 
       //for (i = 0; i < num; i++) {
       //	printf("%02X ", buf[i] & 255);
@@ -53,86 +53,133 @@ int main ()
       //printf("\n");
     }
     // check if any input on stdin
-    while ((c = get_keystroke ()) >= 32)
+    while((c = get_keystroke()) >= 32)
     {
-      printf ("\ngot key '%c', sending...\n", c);
+      printf("\ngot key '%c', sending...\n", c);
       buf[0] = c;
-      for (i = 1; i < 64; i++)
+      for(i = 1; i < 64; i++)
       {
         buf[i] = 0;
       }
-      rawhid_send (0, buf, 64, 100);
+      rawhid_send(0, buf, 64, 100);
     }
   }
 }
 
 
-static char get_keystroke (void)
+static char get_keystroke(void)
 {
-  if (_kbhit ())
+  if(_kbhit())
   {
-    char c = _getch ();
-    if (c >= 32) return c;
+    char c = _getch();
+    if(c >= 32) return c;
   }
   return 0;
 }
 
 
-static void parse_packet (uint8_t* p)
+static void parse_packet(uint8_t* p)
 {
-  if (*p == 0xcc)
+  if(*p == 0xcc)
   {
-    printf ("Command! ");
+    printf("Command! ");
     uint8_t* c;
     c = (p + 1);
-    parse_command (c);
+    parse_command(c);
   }
-  else if (*p == 0x00)
+  else if(*p == 0x00)
   {
     uint8_t len = *(p + 1);
-    printf ("Measurement! p: 0x%02x, len: %d, p+len: 0x%02x\n", *p, len, *(p + len));
+    printf("Measurement! p: 0x%02x, len: %d, p+len: 0x%02x\n", *p, len, *(p + len));
   }
   else
   {
-    printf ("Not recognized packet format!\n");
+    printf("Not recognized packet format!\n");
   }
 }
 
-static void parse_command (uint8_t* c)
+static void parse_command(uint8_t* c)
 {
   char cmd = (char)*c;
-  printf ("code: %c... ", cmd);
-  switch (cmd)
+  printf("code: %c... ", cmd);
+  switch(cmd)
   {
-    case 'c':
-      printf ("calibration!!\n");
+    case 'e':
+      printf("E string!\n");
+      break;
+
+    case 'g':
+      printf("G string!\n");
+      break;
+
+    case 'r':
+      printf("RANGES calib!!\n");
+      state = STATE_CALIB_RANGES;
+      display_help();
+      break;
+
+    case 't':
+      printf("TOUCH calib!!\n");
+      state = STATE_CALIB_TOUCH;
       break;
 
     case 'm':
-      printf ("measurement!!\n");
+      printf("MEASUREMENT!!\n");
+      state = STATE_MEASURING;
       break;
 
     case 'h':
-      printf ("help!!\n");
+      printf("HELP!!\n");
+      display_help();
       break;
 
     case 'x':
-      printf ("exit!!\n");
+      printf("EXIT!!\n");
+      state = STATE_IDLE;
       break;
 
     default:
-      printf ("something else!!\n");
+      printf("something else!!\n");
+      state = STATE_IDLE;
       break;
   }
 }
 
 
-static void print_help ()
+static void display_help()
 {
-  printf ("To navigate here:\n"
-          "'r' : calibrate string RANGES\n"
-          "'t' : calibrate TOUCH thresholds\n"
-          "'m' : start MEASUREMENTS\n"
-          "'h' : display HELP\n"
-          "'x' : EXIT program\n");
+  switch(state)
+  {
+    case STATE_MEASURING:
+    case STATE_IDLE:
+      printf("To navigate here:\n"
+             "'r' : calibrate string RANGES\n"
+             "'t' : calibrate TOUCH thresholds\n"
+             "'m' : start MEASUREMENTS\n"
+             "'h' : display HELP\n"
+             "'x' : EXIT program\n");
+      break;
+
+    case STATE_CALIB_RANGES:
+      printf("Ranges calibration:\n"
+             "'e' : calibrate E string\n"
+             "'g' : calibrate G string\n"
+             "'v' : view current calibration values\n"
+             "'h' : display this help\n"
+             "'x' : exit to main menu\n");
+      break;
+
+    case STATE_CALIB_TOUCH:
+      printf("Touch calibration:\n"
+             "'e' : set touch threshold on E string\n"
+             "'g' : set touch threshold on G string\n"
+             "'v' : view current threshold values\n"
+             "'h' : display this help\n"
+             "'x' : exit to main menu\n");
+      break;
+
+    case STATE_ERROR:
+    default:
+      break;
+  }
 }
--- rawHID2OSC/rawHID2OSC.h
+++ rawHID2OSC/rawHID2OSC.h
@@ -4,12 +4,13 @@
 static char get_keystroke (void);
 static void parse_packet (uint8_t* p);
 static void parse_command (uint8_t* c);
-static void print_help (void);
+static void display_help (void);
 
 enum machine_state
 {
   STATE_IDLE,
-  STATE_CALIBRATING,
+  STATE_CALIB_RANGES,
+  STATE_CALIB_TOUCH,
   STATE_MEASURING,
   STATE_ERROR
 };
